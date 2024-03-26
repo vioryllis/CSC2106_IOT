69d8012b61235c20d0ce7672d183a47966cc55f9
vioryllis <jedzvioletear@gmail.com>
2024-03-26 11:56:59 +0800

    setup db for app

--- iot/iot/settings.py
+++ iot/iot/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'iot_app.apps.IotAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'iot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
--- iot/iot_app/models.py
+++ iot/iot_app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, floor=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not name:
+            raise ValueError('The Name field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, floor=floor, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    class Meta:
+        db_table = "user"
+        # managed = False
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    floor = models.CharField(max_length=50)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Plant(models.Model):
+    class Meta:
+        db_table = "plant"
+        # managed = False
+    plant_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='plants')
+    name = models.CharField(max_length=100)
+    floor = models.CharField(max_length=50)
+    public = models.BooleanField(default=False)
+    last_watered = models.DateTimeField(default=timezone.now)
+    last_fertilized = models.DateTimeField(default=timezone.now)
+    auto_system = models.BooleanField(default=False)
+    min_water_level = models.DecimalField(max_digits=5, decimal_places=2)
+    amt_to_water = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class SensorData(models.Model):
+    class Meta:
+        db_table = "sensordata"
+        # managed = False
+    sensor_data_id = models.AutoField(primary_key=True)
+    water_level = models.DecimalField(max_digits=5, decimal_places=2)
+    nutrient_level = models.DecimalField(max_digits=5, decimal_places=2)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name='sensor_data')
+    
+    def __str__(self):
+        return f"{self.plant.name} Sensor Data"
